a63c3420b2af459540c917574e6716ca6391eeb5
GqIqI <472515111@QQ.COM>
2018-07-13 23:25:02 +0800

    commit

--- 10-html5/9_webworker/main.js
+++ 10-html5/9_webworker/main.js
@@ -2,9 +2,10 @@
  * Created by apple on 18/1/16.
  */
 var worker = new Worker('task.js');
-worker.onmessage = function(e){
+worker.onmessage = function(e){//接收
     console.log(e.data);
 };
-worker.postMessage('aaaaaaaa');
+worker.postMessage('aaaaaaaa');//抛出知识
 
 console.log('bbbbb');
+//相当于异步加载
